c7885ac62a94965921a6e4383c8c11c215f16d05
OlehPetryshyn <olegpetryshen@gmail.com>
2023-04-03 23:53:26 +0300

    Commit dz-5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int num1 = 7;
+        int num2 = 10;
+        int num3 = 11;
+        int num4 = 17;
+        int sum1 = num1 + num4;
+        int sum2 = num3 + num2;
+        String result = sum1 > sum2 ? "True" : "False";
+        System.out.println(result);
+        sum1++;
+        sum2-= 2;
+        // Можна зменшити число на 2 використовуючи ще такий спосіб  (sum2-- двічі)
+        System.out.println(sum1);
+        System.out.println(sum2);
+
+        String result1 = sum1 > sum2 ? "True" : "False";
+        System.out.println(result1);
+        String result3 = (sum1 % 2 == 0) || (sum2 % 2 == 0) ? "True" : "False";
+        System.out.println(result3);
     }
 }
